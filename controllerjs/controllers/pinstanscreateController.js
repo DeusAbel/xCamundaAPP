18c90f1aa27224efed8eac64e29c6c9dbccc7056
gary <gary>
2017-08-08 14:33:45 -0500

    modificaciones iniciar proceso

--- controllerjs/controllers/pinstanscreateController.js
+++ controllerjs/controllers/pinstanscreateController.js
@@ -1,43 +1,74 @@
 var app = angular.module('xCamundaAPP');
-app.controller('pinstanscreateController', function ($scope){
+app.controller('pinstanscreateController', function ($scope) {
 
     console.log("instanscreateController");
 
-      $scope.pinstanVariables = [
+    $scope.pinstanVariables = [
         {
-            "nombre" : "Nombre",
-            "tipo" : "String",
-            "valor" : "Gary"
+            "nombre": "Nombre",
+            "tipo": "String",
+            "valor": "Gary",
+            "modificar": true
         },
         {
-            "nombre" : "monto",
-            "tipo" : "int",
-            "valor" : "1000"
+            "nombre": "monto",
+            "tipo": "int",
+            "valor": "1000",
+            "modificar": true
         }
     ];
 
-    $scope.tipoVariable=[
+    $scope.tipoVariable = [
         {
-            "valor" : "int",
-            "texto" : "Numero"
+            "valor": "int",
+            "texto": "Numero"
         },
         {
-            "valor" : "String",
-            "texto" : "Texto"
+            "valor": "String",
+            "texto": "Texto"
         },
         {
-            "valor" : "date",
-            "texto" : "Fecha"
+            "valor": "date",
+            "texto": "Fecha"
         }
     ];
 
-    $scope.ModificarVariable = function(){
-        it('should toggle button', function() {
-            expect(element(by.css('button')).getAttribute('disabled')).toBeFalsy();
-            element(by.model('checked')).click();
-            expect(element(by.css('button')).getAttribute('disabled')).toBeTruthy();
-        });
+    $scope.bolModificar = true;
+    $scope.btntextoGuardarVariable = "Modificar";
+
+    $scope.ModificarVariable = function (dato) {
+        //        it('should toggle button', function() {
+        //            expect(element(by.css('button')).getAttribute('disabled')).toBeFalsy();
+        //            element(by.model('checked')).click();
+        //            expect(element(by.css('button')).getAttribute('disabled')).toBeTruthy();
+        //        });
+        //alert("modificar");
+        //        if ($scope.bolModificar) {
+        //            $scope.bolModificar=false;
+        //            $scope.btntextoGuardarVariable="Guardar";
+        //        }
+        //        else
+        //            {
+        //                $scope.bolModificar=true;
+        //            $scope.btntextoGuardarVariable="Modificar";
+        //            }
+
+        var pos = $scope.pinstanVariables.indexOf(dato);
+        
+        //console.log($scope.pinstanVariables[0].modificar);
+        
+                if ($scope.pinstanVariables[0].modificar) {
+                    $scope.bolModificar=false;
+                    $scope.btntextoGuardarVariable="Guardar";
+                }
+                else
+                    {
+                        $scope.bolModificar=true;
+                    $scope.btntextoGuardarVariable="Modificar";
+                    }
+
     };
+
 
 
 });
