7ff5b5aa1e0518b9337abbf848a92de3db8f543b
ABEL-PORTATIL\Abel <abel.harc@live.com>
2017-08-08 14:30:22 -0500

    plantilla de servicio completo. Login a espera de servicio real.

--- controllerjs/controllers/loginController.js
+++ controllerjs/controllers/loginController.js
@@ -1,18 +1,21 @@
 var app = angular.module('xCamundaAPP');
 
-app.controller('loginController',function loginController($scope){
+app.controller('loginController',function loginController($scope, loginService){
 	
 	$scope.usuario = {
 			"id" : "0",
 	        "nombre" : "anonimo",
-			"clave" : "anonimo"
+			"clave" : "anonimo",
+			"nombreCompleto" : "usuario aonimo"
 	};
 
 
 	$scope.loginSrv = function(){
-        console.log($scope.usuario.nombre + "/"+ $scope.usuario.clave);
-        if ($scope.usuario.nombre=="abel" && $scope.usuario.clave=="1234"){
-			console.log("Logeado con exito");
+        //console.log($scope.usuario.nombre + "/"+ $scope.usuario.clave);
+        //if ($scope.usuario.nombre=="abel" && $scope.usuario.clave=="1234"){
+		if (loginService.loginUser($scope.usuario)!=null){
+			console.log("Logeado con exito"+ loginService.usuario.nombreCompleto);
+
 		}else{
 			console.log("Error en el logeado");
 		}
--- controllerjs/services/loginService.js
+++ controllerjs/services/loginService.js
@@ -1,15 +1,15 @@
-var app = angular.module('loginService');
+var app = angular.module('xCamundaAPP');
 
-app.factory('loginRequest', function($http){
-		var path = "http://localhost/usuarios/";
-		
-    this.usuario = [
+app.service('loginService', function($http){
+	var path = "http://localhost/usuarios/";
+
+    this.usuario = 
         {
             "id" : "0",
-            "nombre" : "anonimo",
-            "clave" : "anonimo"
-        }
-    ];
+            "nombre" : "Roycer",
+			"clave" : "1234",
+			"nombreCompleto" : "Roycer Gay"
+        };
 	var myHeaders = {
 			'accept': 'application/json',
 			'accept-encoding': 'gzip, deflate',
@@ -18,21 +18,19 @@ app.factory('loginRequest', function($http){
 			'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
 	}
 		
-	this.UpdateUser = function(usuario){
-			var global = $http.post(path + 'checkLogin', user, config);
-			if (global!=null){
-				this.usuario = usuario;
-				return this.usuario
-			}
-
-
-
-		var res = $http.post(path, dataObj);
-		res.success(function(data, status, headers, config) {
-			$scope.message = data;
-		});
-		res.error(function(data, status, headers, config) {
-			alert( "failure message: " + JSON.stringify({data: data}));
-		});	
+	this.loginUser = function(user){
+		//var global = $http.post(path+'id', user, config);
+		/*if (global!=null){
+			this.usuario = user;
+			return this.usuario
+		}else{
+			return null;
+		}*/
+		console.log(user.nombre + "/" + this.usuario.nombre);
+		if (user.nombre == this.usuario.nombre && user.clave == this.usuario.clave)
+			return this.usuario;
+		else
+			return null;
 	}
+
 });
