2af9c449272324b82dc2f1c5e12b6d9c0639305e
gary <gary>
2017-08-11 12:01:20 -0500

    tasklist cargar variables

--- controllerjs/controllers/tasklistController.js
+++ controllerjs/controllers/tasklistController.js
@@ -4,40 +4,8 @@ app.controller('tasklistController', function ($scope, tasklistService) {
     console.log("tasklistController");
 
     $scope.tareasXInstancia = [];
-    //    $scope.tareasXInstancia = [
-    //        {
-    //            "id": "0c5d9601-7861-11e7-b54b-002100a5c998",
-    //            "name": "usuario",
-    //            "description": null,
-    //            "priority": 50,
-    //            "duedate": null,
-    //            "createtime": 1501774387271,
-    //            "processname": "proceso:4:eb1ba39a-7860-11e7-b54b-002100a5c998",
-    //            "taskuniquename": "Task_0xn85j9",
-    //            "assignee": null,
-    //            "starttime": null,
-    //            "endtime": null
-    //        }
-    //    ];
-
-    $scope.CompletarTarea = function (dato) {
-        var pos = $scope.tareasXInstancia.indexOf(dato);
-    };
-
-    $scope.pinstanVariables = [
-        {
-            "nombre": "Nombre",
-            "tipo": "String",
-            "valor": "Gary",
-            "modificar": true
-        },
-        {
-            "nombre": "monto",
-            "tipo": "int",
-            "valor": "1000",
-            "modificar": true
-        }
-    ];
+    
+    $scope.pinstanVariables = [];
 
     $scope.tipoVariable = [
         {
@@ -54,14 +22,14 @@ app.controller('tasklistController', function ($scope, tasklistService) {
         }
     ];
 
+    $scope.CompletarTarea = function (dato) {
+        var pos = $scope.tareasXInstancia.indexOf(dato);
+        
+
+    };
+
     $scope.ModificarVariable = function (dato) {
-        //        it('should toggle button', function() {
-        //            expect(element(by.css('button')).getAttribute('disabled')).toBeFalsy();
-        //            element(by.model('checked')).click();
-        //            expect(element(by.css('button')).getAttribute('disabled')).toBeTruthy();
-        //        });
         var pos = $scope.pinstanVariables.indexOf(dato);
-        //console.log($scope.pinstanVariables[0].modificar);
         if ($scope.pinstanVariables[pos].modificar) {
             $scope.pinstanVariables[pos].modificar = false;
         } else {
@@ -85,21 +53,27 @@ app.controller('tasklistController', function ($scope, tasklistService) {
 
         $scope.pinstanVariables.push($scope.nuevaVariable)
     };
-    
-     $scope.ListarTareas = function (instanciaId) {
+
+    $scope.ListarTareas = function (instanciaId) {
         tasklistService.getTareas(instanciaId).success(function (data) {
             $scope.tareasXInstancia = data;
-            console.log($scope.tareasXInstancia);
+            //console.log($scope.tareasXInstancia);
+        });
+    };
+    
+    $scope.CargarVariablesInstancia = function (instanciaId){
+        tasklistService.getVariablesXInstancia(instanciaId).success(function(data){
+           $scope.pinstanVariables = data;
         });
     };
 
     $scope.$on('instanciaId', function (evt, msg) {
         console.log(msg);
         $scope.ListarTareas(msg);
-
+        $scope.CargarVariablesInstancia(msg);
     });
 
-   
+
 
 
 });
--- controllerjs/services/tasklistService.js
+++ controllerjs/services/tasklistService.js
@@ -1,14 +1,17 @@
 var app = angular.module('xCamundaAPP');
 
-app.service('tasklistService', function($http){
-    
-	var path = "http://localhost:8080/sig/camunda/tareas/listar/instancia"; 
-	
-     return {
-        	getTareas: function(instanciaId){
-        		global = $http.get(path + "/" + instanciaId);
-        		return global;
-        	}
-        }
+app.service('tasklistService', function ($http) {
 
+    var path = "http://localhost:8080/sig/camunda";
+
+    return {
+        getTareas: function (instanciaId) {
+            global = $http.get(path + "/tareas/listar/instancia/" + instanciaId);
+            return global;
+        },
+        getVariablesXInstancia: function (instanciaId) {
+            global = $http.get(path + "/instancias/instancia/" + instanciaId + "/variables/");
+            return global;
+        }
+    }
 });
